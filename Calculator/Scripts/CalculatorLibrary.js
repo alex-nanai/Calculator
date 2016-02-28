dc2e137f504c5a31aac9b27f37a011f670e34271
Alexander Nanay <alexander.nanay@hotmail.com>
2016-02-28 23:05:30 +0200

    Initial commit

---
+++ Calculator/Scripts/CalculatorLibrary.js
@@ -0,0 +1,23 @@
+﻿var txtInput;
+var btnPlus;
+
+function initialize() {
+    txtInput = document.getElementById("txtInput");
+    txtResult = document.getElementById("txtResult");
+    btnPlus = document.getElementById("btnPlus");
+    for (var i = 0; i < 10; i++) {
+        document.getElementById("btn"+i).addEventListener("click", numberClick, false);
+    }
+    btnPlus.addEventListener("click", sumClick, false);
+}
+
+function numberClick() {
+    var buttonClickedText = this.innerText;
+    txtInput.value = txtInput.value == '0' ?
+        buttonClickedText :
+        txtInput.value + buttonClickedText;
+}
+
+function sumClick() {
+    txtResult.value = String(parseInt(txtResult.value) + parseInt(txtInput.value));
+}
---
+++ Calculator/Scripts/tests.js
@@ -0,0 +1,21 @@
+﻿module("Calculator test suite", { setup: function () { initialize(); }});
+
+test("Number button click should add number to txtInput", function () {
+    expect(20);
+    for (var i = 0; i < 10; i++) {
+        var btn = document.getElementById("btn" + i);
+        QUnit.triggerEvent(btn, "click");
+        var value = txtInput.value;
+        var lastDigit = value[value.length-1];
+        equal(lastDigit, i);
+        equal(value.length, i<2 ? 1 : i);
+    }
+});
+
+test("Add test", function () {
+    expect(1);
+    txtInput.value = "20";
+    txtResult.value = "30";
+    QUnit.triggerEvent(btnPlus, "click");
+    equal(txtResult.value, "50");
+});
